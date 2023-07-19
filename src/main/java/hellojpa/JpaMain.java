5959cc05b661e882da8bf762656445ccc755044b
callisto <comportone90@naver.com>
2023-07-19 11:34:13 +0900

    cache clear

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Arrays;
 import java.util.List;
 
 public class JpaMain {
@@ -11,24 +12,22 @@ public class JpaMain {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 		EntityManager entityManager = emf.createEntityManager();
-
-
 		EntityTransaction transaction = entityManager.getTransaction();
+
 		transaction.begin();
 		try {
+			Member findMember = entityManager.find(Member.class, 102L);
+			findMember.setName("HelloZ");
 
-			//Member findMember = entityManager.find(Member.class, 1L);
-			//findMember.setName("HelloA");
-			List<Member> resultList = entityManager.createQuery("select m from Member as m", Member.class)
-				.setFirstResult(2)
-				.setMaxResults(3)
-				.getResultList();
+			System.out.println("==============================");
 
-			for (Member member : resultList) {
-				System.out.println("member.getName() = " + member.getName());
-			}
+			/*
+			//회원 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태로 만든다.
+			entityManager.detach(member);
 
-			//entityManager.persist(findMember);
+			//객체를 삭제한 상태
+			//entityManager.remove(member);
+			*/
 
 			transaction.commit();
 		}catch(Exception e) {
