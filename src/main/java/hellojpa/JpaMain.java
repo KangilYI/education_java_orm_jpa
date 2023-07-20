5b4057c71ace3865fe5c7a8c66e7a99f531cb2a6
callisto <comportone90@naver.com>
2023-07-20 11:03:13 +0900

    23.07.20 inlean JPA 강의 영속성 관리 flush

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,37 +4,45 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.Arrays;
-import java.util.List;
 
 public class JpaMain {
 
-	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-		EntityManager entityManager = emf.createEntityManager();
-		EntityTransaction transaction = entityManager.getTransaction();
+	/*
+	//회원 엔티티를 영속성 컨텍스트에 등록
+	entityManager.persist(memeber);
+	entityManager.find(T.class, Long);
 
+	//회원 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태로 만든다.
+	entityManager.detach(member);
+
+	//객체를 삭제한 상태
+	//entityManager.remove(member);
+	*/
+
+	public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+	public static final EntityManager entityManager = emf.createEntityManager();
+	public static final EntityTransaction transaction = entityManager.getTransaction();
+
+	public static void main(String[] args) {
 		transaction.begin();
+		//플러시
+		JPAFlush();
+		emf.close();
+	}
+
+	public static void JPAFlush() {
 		try {
-			Member findMember = entityManager.find(Member.class, 102L);
-			findMember.setName("HelloZ");
+			Member member = new Member(104L, "HelloC");
+			entityManager.persist(member);
+
+			entityManager.flush();
 
 			System.out.println("==============================");
-
-			/*
-			//회원 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태로 만든다.
-			entityManager.detach(member);
-
-			//객체를 삭제한 상태
-			//entityManager.remove(member);
-			*/
-
 			transaction.commit();
 		}catch(Exception e) {
 			transaction.rollback();
 		} finally {
 			entityManager.close();
 		}
-		emf.close();
 	}
 }
